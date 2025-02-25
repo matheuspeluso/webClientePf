2845d342d7f22843a34c11a61b8b2ee133262ddb
Matheus Peluso <52898194+matheuspeluso@users.noreply.github.com>
2025-02-24 23:17:09 -0300

    Implementação de componente de Atualização de cliente

--- src/app/components/pages/edicao-clintes/edicao-clintes.component.ts
+++ src/app/components/pages/edicao-clintes/edicao-clintes.component.ts
@@ -1,11 +1,75 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'app-edicao-clintes',
-  imports: [],
+  imports: [
+    FormsModule, 
+    CommonModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './edicao-clintes.component.html',
   styleUrl: './edicao-clintes.component.css'
 })
 export class EdicaoClintesComponent {
 
+  mensagem: string = '';
+  id: string = '';
+
+  constructor( //injeção de dependência para poder usar o HttpClient
+    private httpClient: HttpClient,
+    private activatedRoute: ActivatedRoute //injeção de dependência para poder pegar o id da URL
+  ) { }
+
+  ngOnInit(){ //realizando a requisição de consulta por id ao iniciar o componente
+    //capturando o id do cliente enviado na URL
+    this.id = this.activatedRoute.snapshot.paramMap.get('id') as string;
+
+    //consultando o cliente na API através do ID
+    this.httpClient.get<any>('http://localhost:9000/clientes/consultar/' + this.id)
+      .subscribe({
+        next: (data) => {
+
+          if (data.dataNascimento) {
+            // Extraindo apenas a parte da data (yyyy-MM-dd)
+            data.dataNascimento = data.dataNascimento.split(' ')[0];
+          }
+          //preencher o formulário com os dados do cliente
+          this.formulario.patchValue(data);
+        },
+      });
+  }
+
+  
+  get f(){ 
+    return this.formulario.controls;
+  }
+  
+  estados = ['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO'];
+  
+  formulario = new FormGroup({
+    nome: new FormControl('', [Validators.required, Validators.minLength(8), Validators.maxLength(100)]),
+    email: new FormControl('', [Validators.required, Validators.email, Validators.email]),
+    cpf: new FormControl('', [Validators.required, Validators.pattern('^(\\d{3}\\.\\d{3}\\.\\d{3}-\\d{2}|\\d{11})$')]),
+    dataNascimento: new FormControl('', [Validators.required]),
+  });
+
+  editarCliente() {
+    
+    //enviando uma requisição PUT para a API (atualizar o cliente)
+    this.httpClient.put<any>('http://localhost:9000/clientes/alterar/'+ this.id,
+      this.formulario.value, //enviando os dados do formulário
+    ).subscribe({
+      next: (data) => {
+        //exibindo a mensagem obtida da API
+        this.mensagem = data.mensagem;
+      }
+    })
+
+  }
 }
